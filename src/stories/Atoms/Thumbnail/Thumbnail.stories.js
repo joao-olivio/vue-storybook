893886b0ee4666aec285c5cb1a273defcdbe7045
tulio <tulioam9@gmail.com>
2022-07-20 18:23:59 -0300

    #25-offer-thumbnail - created variation for the thumbnail component named offer

--- src/stories/Atoms/Thumbnail/Thumbnail.stories.js
+++ src/stories/Atoms/Thumbnail/Thumbnail.stories.js
@@ -1,20 +1,32 @@
-import Thumbnail from './Thumbnail.vue';
+import Thumbnail from "./Thumbnail.vue";
 
 export default {
-    title: "Atoms/Thumbnail",
-    component: Thumbnail
+  title: "Atoms/Thumbnail",
+  component: Thumbnail,
 };
 
 const Template = (args) => ({
-    components: { Thumbnail },
-    setup() {
-      return { args };
-    },
-    template: '<thumbnail v-bind="args"/>',
+  components: { Thumbnail },
+  setup() {
+    return { args };
+  },
+  template: '<thumbnail v-bind="args"/>',
 });
-
 
 export const Post = Template.bind({});
 Post.args = {
-    image: 'https://res.cloudinary.com/weigelcinthya/image/upload/v1658183370/ross-parmly-rf6ywHVkrlY-unsplash_jfoneh.jpg'
-}
+  image:
+    "https://res.cloudinary.com/weigelcinthya/image/upload/v1658183370/ross-parmly-rf6ywHVkrlY-unsplash_jfoneh.jpg",
+};
+
+export const Offer = Template.bind({});
+Offer.args = {
+  image:
+    "https://res.cloudinary.com/weigelcinthya/image/upload/v1658183370/ross-parmly-rf6ywHVkrlY-unsplash_jfoneh.jpg",
+  offer: {
+    city: "Superior",
+    initialDate: "18.09",
+    finalDate: "21.09",
+    price: 50000,
+  },
+};
--- src/stories/Atoms/Thumbnail/Thumbnail.vue
+++ src/stories/Atoms/Thumbnail/Thumbnail.vue
@@ -1,27 +1,126 @@
 <template>
-    <div class="thumbnail">
-        <img class="thumbnail__image" :src="image"/>
+  <div
+    class="thumbnail"
+    :class="[props.offer ? 'thumbnail--offer' : 'thumbnail--post']"
+  >
+    <img class="thumbnail__image" :src="image" />
+    <div class="thumbnail-offer" v-if="props.offer">
+      <span class="thumbnail-offer__item">
+        <div class="thumbnail-offer__item__info">
+          <small>City</small>
+          <small>
+            <b>{{ props.offer.city }}</b>
+          </small>
+        </div>
+      </span>
+      <span class="thumbnail-offer__item d-none-mobile">
+        <div class="vertical-line"></div>
+        <div class="thumbnail-offer__item__info">
+          <small>Dates</small>
+          <small>
+            <b>{{ props.offer.initialDate }} - {{ props.offer.finalDate }}</b>
+          </small>
+        </div>
+      </span>
+      <span class="thumbnail-offer__item">
+        <div class="vertical-line"></div>
+        <div class="thumbnail-offer__item__info">
+          <small> Price</small>
+          <small>
+            <b>${{ props.offer.price }}</b>
+          </small>
+        </div>
+      </span>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-    const props = defineProps({
-        image: String
-    });
+type Offer = {
+  city: string;
+  initialDate: string;
+  finalDate: string;
+  price: string;
+};
+
+const props = defineProps({
+  image: String,
+  offer: {
+    type: Object as () => Offer,
+  },
+});
 </script>
 
 <style lang="scss">
-    .thumbnail {
-        border-radius: 15px;
-        max-width: 370px;
-        height: 250px;
-        position: relative;
-        overflow: hidden;
+.thumbnail {
+  border-radius: 15px;
+  position: relative;
+  overflow: hidden;
 
-        &__image {
-            position: absolute;
-            width: 100%;
-            height: 100%;
-        }
+  &--post {
+    width: 328px;
+    height: 224px;
+
+    @media screen and (min-width: 768px) {
+      width: 370px;
+      height: 250px;
     }
+  }
+
+  &--offer {
+    width: 328px;
+    height: 190px;
+
+    @media screen and (min-width: 768px) {
+      width: 570px;
+      height: 330px;
+    }
+  }
+
+  &__image {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
+  &-offer {
+    padding: 15px;
+    border-radius: 15px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: 0 15px 15px;
+    background-color: var(--default-white);
+    opacity: 0.8;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    @media screen and (min-width: 768px) {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    &__item {
+      display: flex;
+      gap: 5px;
+
+      &__info {
+        display: flex;
+        flex-direction: column;
+      }
+    }
+  }
+}
+
+.vertical-line {
+  border-left: 1px solid #999999;
+  height: 100%;
+}
+
+.d-none-mobile {
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: flex;
+  }
+}
 </style>
